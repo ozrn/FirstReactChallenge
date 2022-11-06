b236af101971fb765e2dbc3c545865817d58bf6a
ozrn <ozerenkevser92@gmail.com>
2022-11-06 14:32:07 +0300

    Render components in app component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Navbar from "./Navbar"
-import Main from "./Main"
+import App from "./App"
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <div>
-    <Navbar />
-    <Main />
-  </div>    
+  <App />
 )
